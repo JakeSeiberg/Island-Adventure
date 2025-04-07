f3d60ae35659a5e93a8ca2a35ad0d9f68cf309b7
Tyler Garvey <tmgarv26@colby.edu>
2025-04-07 16:42:01 +0200

    working on interactions

---
+++ Assets/PlayerInteraction/wormInteract.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class wormInteract : MonoBehaviour
+{
+    // Static method that can be called without an instance of wormInteract
+    public static void wormCollected()
+    {
+        print("Hey");
+    }
+}
