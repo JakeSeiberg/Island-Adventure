372c5647876e4473fc5632a67364522150c81925
Jack Hobday <jackhobday11@gmail.com>
2025-04-24 16:00:52 +0200

    Added StartMenu

---
+++ Assets/Scripts/StartMenu/StartMenu.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainWorld");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
+
