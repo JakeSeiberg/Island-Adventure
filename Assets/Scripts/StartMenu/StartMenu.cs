ba4b4d2166a96ca42f2c14e5f37bac1a694859af
Tyler Garvey <tmgarv26@colby.edu>
2025-04-25 19:47:14 +0200

    fixed mainmenu scaling issue & made it go to the cutscene

--- Assets/Scripts/StartMenu/StartMenu.cs
+++ Assets/Scripts/StartMenu/StartMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("MainWorld");
+        SceneManager.LoadScene("CutScene");
     }
 
     public void QuitGame()
